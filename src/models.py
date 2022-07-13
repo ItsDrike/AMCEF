4bb02e0df9612da213782cb61f1085065fa9ecf8
ItsDrike <itsdrike@protonmail.com>
2022-07-13 22:31:56 +0200

    Specify type hints for SQLAlchemy models

--- src/models.py
+++ src/models.py
@@ -1,3 +1,6 @@
+# type: ignore  # SQLAlchemy models specify Columns, model instances will not be of Column type
+from typing import Optional
+
 from sqlalchemy import Boolean, Column, Integer, String
 
 from src.utils.database import Base
@@ -6,15 +9,15 @@ from src.utils.database import Base
 class Post(Base):
     __tablename__ = "posts"
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer)
-    title = Column(String)
-    body = Column(String)
+    id: int = Column(Integer, primary_key=True, index=True)
+    user_id: int = Column(Integer)
+    title: str = Column(String)
+    body: str = Column(String)
 
 
 class Member(Base):
     __tablename__ = "members"
 
-    member_id = Column(Integer, primary_key=True, index=True)
-    key_salt = Column(String, nullable=True)
-    is_admin = Column(Boolean, default=False)
+    member_id: int = Column(Integer, primary_key=True, index=True)
+    key_salt: Optional[str] = Column(String, nullable=True)
+    is_admin: bool = Column(Boolean, default=False)
--- src/utils/auth.py
+++ src/utils/auth.py
@@ -12,7 +12,7 @@ from src.constants import Server
 
 
 class AuthState(Enum):
-    """Represents possible outcomes of a user attempting to authorize."""
+    """Represents possible outcomes of a member attempting to authorize."""
 
     NO_TOKEN = (
         "There is no token provided, provide one in an Authorization header in the format 'Bearer {your token here}'."
@@ -44,16 +44,16 @@ class JWTBearer(HTTPBearer):
             raise HTTPException(403, AuthState.INVALID_TOKEN.value)
 
         db_session = request.state.db_session
-        user = await crud.get_member(db_session, int(token_data["id"]))
+        member = await crud.get_member(db_session, int(token_data["id"]))
 
-        if user is None or user.key_salt != token_data["salt"]:
+        if member is None or member.key_salt != token_data["salt"]:
             raise HTTPException(403, AuthState.INVALID_TOKEN.value)
-        elif self.require_admin and not user.is_admin:
+        elif self.require_admin and not member.is_admin:
             raise HTTPException(403, AuthState.NEEDS_ADMIN.value)
 
-        # Token is valid, store the user_id and is_admin data into the request
-        request.state.user_id = user.user_id
-        request.state.is_admin = user.is_admin
+        # Token is valid, store the member_id and is_admin data into the request
+        request.state.member = member.member_id
+        request.state.is_admin = member.is_admin
 
         return credentials
 
@@ -72,7 +72,7 @@ def _make_member_token(member_id: int) -> tuple[str, str]:
 
 
 async def reset_member(db_session: AsyncSession, member_id: int, *, is_admin: bool = False) -> schemas.TokenMemberData:
-    """Generate a new API token for given member and update the user data to match."""
+    """Generate a new API token for given member and update the member data to match."""
     token, token_salt = _make_member_token(member_id)
     await crud.update_member(db_session, member_id, is_admin=is_admin, key_salt=token_salt)
     ret = schemas.TokenMemberData(member_id=member_id, api_token=token, is_admin=is_admin)
@@ -80,7 +80,7 @@ async def reset_member(db_session: AsyncSession, member_id: int, *, is_admin: bo
 
 
 async def generate_member(db_session: AsyncSession, *, is_admin: bool = False) -> schemas.TokenMemberData:
-    """Generate and store a new member, returning their API token and user id."""
-    new_user = await crud.make_blank_member(db_session)
-    member_data = await reset_member(db_session, new_user.member_id, is_admin=is_admin)
+    """Generate and store a new member, returning their API token and member id."""
+    new_member = await crud.make_blank_member(db_session)
+    member_data = await reset_member(db_session, new_member.member_id, is_admin=is_admin)
     return member_data
