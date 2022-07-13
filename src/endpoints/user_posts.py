000b3bc2edb6b77dd7f1c13cb802f5b47fa8232b
ItsDrike <itsdrike@protonmail.com>
2022-07-13 15:49:32 +0200

    Fall back on API if give post isn't in our database

--- src/endpoints/user_posts.py
+++ src/endpoints/user_posts.py
@@ -1,7 +1,10 @@
+from typing import Optional
+
 import httpx
 from fastapi import APIRouter, HTTPException
 from fastapi.requests import Request
 from fastapi.responses import Response
+from sqlalchemy.ext.asyncio import AsyncSession as DBAsyncSession
 
 from src import crud, models, schemas
 from src.constants import Connection
@@ -37,6 +40,39 @@ async def ensure_valid_user_id(httpx_client: httpx.AsyncClient, user_id: int) ->
     raise HTTPException(422, err_details)
 
 
+async def lookup_post(
+    db_session: DBAsyncSession,
+    httpx_client: httpx.AsyncClient,
+    post_id: int,
+) -> Optional[models.Post]:
+    """
+    Find post of given id in our database, or if it's not present, look it up from API.
+
+    If the post isn't present, the lookup will also store the post into our database for faster future lookups.
+    If the post lookup fails (both from database and API), return None.
+    """
+    db_post = await crud.get_post(db_session, post_id)
+    if db_post is not None:
+        return db_post
+
+    response = await httpx_client.get(f"{Connection.API_BASE_URL}/posts/{post_id}")
+    if response.status_code == 404:
+        return None
+    # Make sure we exit loudly with HTTPError from httpx in case the API fails
+    # this should produce HTTP code 500 when unhandled within route
+    response.raise_for_status()
+
+    post_data = response.json()
+    post_schema = schemas.Post(
+        id=post_data["id"],
+        user_id=post_data["userId"],
+        title=post_data["title"],
+        body=post_data["body"],
+    )
+    db_post = await crud.add_post(db_session, post_schema)
+    return db_post
+
+
 @router.post("/post", response_model=schemas.Post)
 async def create_post(request: Request, data: schemas.PostCreate) -> models.Post:
     db_session = request.state.db_session
@@ -50,9 +86,10 @@ async def create_post(request: Request, data: schemas.PostCreate) -> models.Post
 @router.get("/post/{post_id}", response_model=schemas.Post)
 async def get_post(request: Request, post_id: int) -> models.Post:
     db_session = request.state.db_session
+    httpx_client = request.state.httpx_client
 
-    db_post = await crud.get_post(db_session, post_id)
-    if not db_post:
+    db_post = await lookup_post(db_session, httpx_client, post_id)
+    if db_post is None:
         raise HTTPException(404, "No such post")
     return db_post
 
