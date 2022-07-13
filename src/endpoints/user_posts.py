ece0a1961c62ffb40b284e59792388d3e908e578
ItsDrike <itsdrike@protonmail.com>
2022-07-13 15:26:42 +0200

    Perform user_id validation based on jsonplaceholder API

--- src/endpoints/user_posts.py
+++ src/endpoints/user_posts.py
@@ -1,3 +1,4 @@
+import httpx
 from fastapi import APIRouter, HTTPException
 from fastapi.requests import Request
 from fastapi.responses import Response
@@ -7,10 +8,38 @@ from src import crud, models, schemas
 router = APIRouter(tags=["User posts endpoints"])
 
 
+async def ensure_valid_user_id(httpx_client: httpx.AsyncClient, user_id: int) -> None:
+    """
+    Custom validation logic for user_id using external API.
+
+    We do this here instead of in the Post model schema because we're making an asynchronous
+    request and pydantic doesn't support async validators.
+    """
+    response = await httpx_client.get("https://jsonplaceholder.typicode.com/users")
+    users = response.json()
+    for user in users:
+        if user["id"] == user_id:
+            return
+
+    # Use the same convention as default FastAPI 422 on pydantic validation error
+    err_details = {
+        "detail": [
+            {
+                "loc": ["body", "user_id"],
+                "msg": f"There is no user with ID {user_id}",
+                "type": "value_error",
+            }
+        ]
+    }
+    raise HTTPException(422, err_details)
+
+
 @router.post("/post", response_model=schemas.Post)
 async def create_post(request: Request, data: schemas.PostCreate) -> models.Post:
     db_session = request.state.db_session
+    httpx_client = request.state.httpx_client
 
+    await ensure_valid_user_id(httpx_client, data.user_id)
     db_post = await crud.add_post(db_session, data)
     return db_post
 
