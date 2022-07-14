7f77c7bcd0458fdfdc62129a74fbc99f2b8e9989
ItsDrike <itsdrike@protonmail.com>
2022-07-14 19:35:31 +0200

    Add alembic for easy future migrations

---
+++ alembic/versions/2022_07_14_1928-5e46fb9b6bac_initial_state.py
@@ -0,0 +1,24 @@
+"""Initial state
+
+Revision ID: 5e46fb9b6bac
+Revises:
+Create Date: 2022-07-14 19:28:40.954384
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "5e46fb9b6bac"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
