80df882a935eda6d1adfdc4538de5018b75f3dc7
ItsDrike <itsdrike@protonmail.com>
2022-09-12 16:08:15 +0200

    Don't inform users about IP wide rate-limits
    By default rate-limit buckets add certain headers to the responses that
    inform the client about how many requests they still have in given
    period. In most cases this is a good thing as clients can build software
    that respects these headers and doesn't get into cooldowns.
    
    However with an IP wide bucket, we don't necessarily expect there to
    just be a single computer on that network, meaning showing these headers
    wouldn't really make sense, as other people might use up the requests
    too. Usually, we shouldn't see the IP rate limit getting crossed anyway
    and it's just here to protect us from hitting rate-limits on the the
    external API that we rely on.
    
    If a cooldown is triggered though, a special cooldown header will
    appear, so that the clients can await it.

--- src/utils/ratelimits/ip_bucket.py
+++ src/utils/ratelimits/ip_bucket.py
@@ -1,6 +1,7 @@
 import logging
 
 from fastapi.requests import Request
+from fastapi.responses import Response
 
 from src.utils.ratelimits.abc import RedisBucketBase
 
@@ -18,3 +19,20 @@ class IPRedisBucket(RedisBucketBase[str]):
         host_address, _ = request.client.host
         log.debug(f"Obtained bucket key for rate-limited route: {host_address} (bucket #{self.bucket_no})")
         return host_address
+
+    async def add_headers(self, response: Response, bucket_key: str) -> None:
+        """
+        Override add_headers since we don't want to be informing the user about ip-based rate-limits.
+
+        This also allows us to use this bucket in combination with other buckets, as it won't be
+        overriding headers from them.
+        """
+
+    async def add_cooldown_headers(self, response: Response, remaining_seconds: int) -> None:
+        """
+        Override add_cooldown_headers to change the cooldown header name.
+
+        This avoids conflicts with other buckets, and makes it clear that if this header appears,
+        the API is under a ip-wide cooldown which should be respected.
+        """
+        response.headers.append("Ip-Cooldown-Reset", str(remaining_seconds))
